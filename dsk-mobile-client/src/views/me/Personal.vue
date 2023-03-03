b6003ba34fe1bd9ec88cffc977a589ea6012bb83
LiXiaoDong <292738778@qq.com>
2023-03-03 11:34:13 +0800

    2023年3月3日 11:33:42  me页面更新

--- dsk-mobile-client/src/views/me/Personal.vue
+++ dsk-mobile-client/src/views/me/Personal.vue
@@ -1,61 +1,36 @@
 <template>
   <div>
-    <Vue-imageCutting
-        :picturePath="picturePathData"  
-        :buttonColor="buttonColorData" 
-        :backgroundColor="backgroundColorData" 
-        :iconColor="iconColorData" 
-        :format="formatData" 
-        :crossDomain="crossDomainData" 
-        :maskOpacity="maskOpacityData" 
-        :minZoom="minZoomData" 
-        :maxZoom="maxZoomData" 
-        @confirm="confirmMethod" 
-        @cancel="cancelMethod" 
-        @rotate="rotateMethod" 
-        @restore="restoreMethod" 
-        @loading="loadingMethod" 
-        @touching="touchingMethod" 
-    />
+    <van-nav-bar class="nav" @click-left="$router.go(-1)" left-arrow size:50px/>
+    <van-divider
+  :style="{borderColor: '#d8d8d8' , padding: '0 16px'}"
+>
+  </van-divider>
+
+  <van-image
+  style="box-shadow:0px 1px 10px #333;"
+  radius="5px"
+  width="15vh"
+  height="15vh"
+  fit="cover"
+  src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"
+/>
+
+  <view>
+    <h3>西瓜</h3>
+    <span style=" color: #999;">好好学习，天天向上</span>
+  </view>
+
+
   </div>
 </template>
-<script>
-import VueImageCutting from 'vue-image-cutting'
-import 'vue-image-cutting/lib/VueImageCutting.css'
-Vue.use(VueImageCutting)
-  export default {
-    data () {
-      return {
-        maskOpacityData:0.7,
-        formatData:"jpeg",
-        minZoomData:0.5,
-        crossDomainData:false,
-        maxZoomData:3,
-        iconColorData:"#ffffff",
-        backgroundColorData:'#000000',
-        buttonColorData:'#0e9a00',
-        picturePathData:"../../assets/img/cl.png"
-      }
-    },
-    methods:{
-        touchingMethod(event){
-            console.log(event)
-        },
-        restoreMethod(event){
-            console.log(event)
-        },
-        rotateMethod(event){
-            console.log(event)
-        },
-        cancelMethod(event){
-            console.log(event)
-        },
-        loadingMethod(event){
-            console.log(event)
-        },
-        confirmMethod(event){
-            console.log(event)
-        }
-    }
-  }
+
+<script setup lang="ts">
+
 </script>
+
+<style scoped>
+.nav {
+  --van-nav-bar-arrow-size:	25px;
+  --van-nav-bar-icon-color:	#999;
+}
+</style>
