58072264713e85078013f27d3f8b8970ce40c3c8
xiaoran <413239213@qq.com>
2023-03-08 08:49:10 +0800

    2023年3月8日08:48:54 分类页面

--- dsk-mobile-client/src/views/classification/Classification.vue
+++ dsk-mobile-client/src/views/classification/Classification.vue
@@ -1,13 +1,67 @@
 <template>
     <div>
-      <van-empty>分类，敬请期待</van-empty>
+      <van-nav-bar title="题材" />
+      <div class="div">
+        <div class="div-left" @click="$router.push('/home/more')">
+          <span>人物传记</span>
+          <img src="../../assets/img/dp.png">
+        </div>
+        <div class="div-right">
+          <span>网络小说</span>
+          <img src="../../assets/img/dp.png" alt="">
+        </div>
+      </div>
+      <div class="div">
+        <div class="div-left">
+          <span>人物传记</span>
+          <img src="../../assets/img/dp.png">
+        </div>
+        <div class="div-right">
+          <span>网络小说</span>
+          <img src="../../assets/img/dp.png" alt="">
+        </div>
+      </div>
     </div>
   </template>
   
   <script setup lang="ts">
-  
+import { createApp } from 'vue';
+import { NavBar } from 'vant';
+
+const app = createApp();
+app.use(NavBar);
+
+
   </script>
   
   <style scoped>
-  
+  *{
+    margin: 0px;
+    padding: 0px;
+  }
+  .div{
+    display: flex;
+  }
+.div-left{
+  display: flex;
+  width: 40%;
+  height: 80px;
+  background-color: #f5f7f9;
+  border-radius: 10px;
+  margin-left: 5%;
+  margin-top: 5%;
+}
+span{
+  line-height: 80px;
+}
+.div-right{
+  display: flex;
+  width: 40%;
+  height: 80px;
+  background-color: #f5f7f9;
+  border-radius: 10px;
+  margin-left: 10%;
+  margin-top: 5%;
+
+}
   </style>
